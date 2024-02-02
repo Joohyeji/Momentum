015a16b887e8867f81695187f92e05cd4f3495a8
Joohyeji <rosyjoo1999@gmail.com>
2024-02-02 14:21:14 +0900

    css추가

--- js/background.js
+++ js/background.js
@@ -1,10 +1,12 @@
 const images = [
-    '1.jpg', '2.jpg', '3.jpg'
+    '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'
 ];
 
 const chosenImages = images[Math.floor(Math.random()*images.length)];
 
 const bgImage = document.createElement('img');
+
+bgImage.classList.add('one');
 
 bgImage.src = `img/${chosenImages}`;
 
--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,11 @@ const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
+// todo
+const toDoForm2 = document.querySelector('#todo-form');
+const toDoInput2 = toDoForm2.querySelector('input');
+const toDoList2 = document.querySelector('#todo-list');
+
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
 
@@ -20,6 +25,10 @@ const paintGreetings = () => {
   const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  
+  toDoForm2.classList.remove(HIDDEN_CLASSNAME);
+  toDoList2.classList.remove(HIDDEN_CLASSNAME);
+
 };
 
 // 저장된 로그인 값이 있는지 확인
